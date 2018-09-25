f5ba1d4b8959f8c177c73a5fff2644725a45838b
RonSala <ron.sala@gmail.com>
2018-09-25 06:24:53 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,11 @@
 class FiguresController < ApplicationController
+
   get "/figures/new" do
+    @figures = Figure.all
     erb :"figures/new"
+  end
+
+  get "/figures" do
+    erb :"figures/index"
   end
 end

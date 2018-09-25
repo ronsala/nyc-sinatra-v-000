e5b22947c542642d851aee8d1a96b1de1f205891
RonSala <ron.sala@gmail.com>
2018-09-25 07:36:43 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,6 +3,11 @@ class LandmarksController < ApplicationController
     erb :"landmarks/new"
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/show"
+  end
+
   get "/landmarks" do
     @landmarks = Landmark.all
     erb :"landmarks/index"

4552a270a2f7d43e2358f1f4204cbf13bea2aacd
RonSala <ron.sala@gmail.com>
2018-09-29 14:29:28 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,35 +6,45 @@ class FiguresController < ApplicationController
 
   post "/figures" do
     # binding.pry
-    @figure = Figure.create(:name=> params[:figure][:name])
+    @figure = Figure.create(:name => params["figure"]["name"], :title_ids => params["figure"]["title_ids"], :landmark_ids => params["figure"]["landmark_ids"])
 
-    if !params[:title][:name].empty?
-      @figure.titles << Title.create(params[:figure][:title])
-      @figure.save
+    if !params[:title][:name].empty?       
+      @figure.titles << Title.create(params[:title])    
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
     end
 
-    if !params[:figure][:title_ids].empty?
-      params[:figure][:title_ids].each_with_index do | t, i |
-      @figure.titles << Title.find_by_id(params[:figure][:title_ids][i])
-      end
-      @figure.save
-    end
+    @figure.save
+    redirect '/figures'
+
+    # if !params[:title][:name].empty?
+    #   @figure.titles << Title.create(params[:figure][:title])
+    # end
+
+    # if !params[:figure][:title_ids].empty?
+    #   params[:figure][:title_ids].each_with_index do | t, i |
+    #   @figure.titles << Title.find_by_id(params[:figure][:title_ids][i])
+    #   end
+    #   @figure.save
+    # end
 
     # create a new figure with a landmark
     # if !params[:figure][:landmark_ids].empty?
       # binding.pry
-      params[:figure][:landmark_ids].each_with_index do | l, i |
-      @figure.landmarks << Landmark.find_by_id(params[:figure][:landmark_ids][i])
-      end
-      @figure.save
+      # params[:figure][:landmark_ids].each_with_index do | l, i |
+      # @figure.landmarks << Landmark.find_by_id(params[:figure][:landmark_ids][i])
+      # end
+      # @figure.save
     # end
-  end
+ 
 
-      #create a new figure with a new landmark
+    #    create a new figure with a new landmark
     # if !params[:figure][:landmark][:name].empty? && !params[:figure][:landmark][:year].empty?
-    #   @figure.landmarks << Landmark.create(:name => params[:figure][:landmark][:name], :year_completed => [:figure][:landmark][:year])
+    #   @figure.landmarks << Landmark.create(:name => params[:figure][:landmark][:name], :year_completed => params[:figure][:landmark][:year])
     #   @figure.save
     # end
+  end
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
